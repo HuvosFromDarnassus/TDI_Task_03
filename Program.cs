637d07b3421a059a9ae61df66f1451fcd5984db0
Daniel Tvorun <danya.tvorun.88@mail.ru>
2020-11-16 17:22:53 +0300

    Решение задачи #3

--- Program.cs
+++ Program.cs
@@ -4,9 +4,35 @@ namespace TDI_Task_03
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string password = "password";
+            string userInput;
+
+            bool isContinue = true;
+
+            int tryesCounter = 1;
+
+            while (isContinue)
+            {
+                Console.Write("\nEnter a password: ");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.Write("\nSECRET MESSAGE");
+                    isContinue = false;
+                }
+                else if (tryesCounter == 3)
+                {
+                    isContinue = false;
+                }
+                else
+                {
+                    Console.Write("\nUncorrect password, try again");
+                    tryesCounter++;
+                }
+            }
         }
     }
 }
